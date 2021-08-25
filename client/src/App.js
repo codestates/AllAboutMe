247bb224f6017fab934106657ed393c734afd899
siyoon <younyaho@gmail.com>
2021-08-26 00:16:45 +0900

    [Client] #53 feat:merge signup and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
-import SignUp from './src/pages/signup.js'
-import { BrowserRouter, Switch, Link } from 'react-router-dom';
+import SignUp from './pages/signup.js'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Nav from './pages/nav.js';
 import Footer from './pages/footer.js'
 import Home from './pages/home.js'
+import Login from './pages/login';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/signup'>
+            <SignUp />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Switch>
 
-      <SignUp/>
-      <Nav />
-      <Home />
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+        <footer>
+          <Footer />
+        </footer>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function login(){
+  return (
+    <>
+        <h1>login</h1>
+    </>
+  );
+};
+
+export default login;
