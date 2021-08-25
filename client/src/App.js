afb6443c1c8be612445a4ce0eb8e1602b3139eb8
siyoon <younyaho@gmail.com>
2021-08-25 14:23:52 +0900

    [client] feat:home

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <Home /> */}
+      <Home />
       <footer>
         <Footer />
       </footer>
---
+++ client/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css'
+
+function Home(){
+  return (
+    <div className='home'>
+      <div className='home_container'>
+        <h1>All About Me</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
