0ed71b0682bdbc7f0b619daba8f0f7df2ee6dba1
siyoon <younyaho@gmail.com>
2021-08-25 14:21:28 +0900

    [client] feat:footer

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <Home />
+      {/* <Home /> */}
       <footer>
         <Footer />
-      </footer> */}
+      </footer>
     </div>
   );
 }
---
+++ client/src/pages/footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './footer.css'
+
+function Footer(){
+  return (
+    <>
+    <div>
+        <div className='footer'>
+            <div className='footer_container'>
+                <h3>Contact</h3>
+                    <ul>
+                        <li><a href='https://github.com/loverduck'>Yoo So-yeon</a></li>
+                        <li><a href='https://github.com/james-taeil'>Kim Tae-il</a></li>
+                        <li><a href='https://github.com/cue28'>Jeon Si-yoon</a></li>
+                        <li><a href='https://github.com/soowan-jang'>Jang Soo-wan</a></li>
+                    </ul>
+            </div>
+            <div className='footer_container'>
+                <h3>About Us</h3>
+                    <ul>
+                        <li><a href='https://github.com/codestates/AllAboutMe/wiki'>Wiki</a></li>
+                        <li><a href=''>Client Repository</a></li>
+                        <li><a href=''>Server Repository</a></li>
+                    </ul>
+            </div>
+            <div className='footer_container'>
+                <img src='/logo1.png' alt='logo' />
+            </div>
+            <div className='footer_bottom'>
+                <p>copyright &copy;2021 슬기로운 코딩생활 all right reserved.</p>
+            </div>
+        </div>
+        </div>
+    </>
+  );
+};
+
+export default Footer;
