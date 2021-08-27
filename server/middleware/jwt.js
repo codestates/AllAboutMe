9a85c0823f4649cb345d37e1ee697f726d079346
soyou <loverduck97@naver.com>
2021-08-27 15:09:05 +0900

    [Server] feat: add jwt verify feature

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -5,5 +5,14 @@ module.exports = {
   generateAccessToken: (data) => {
     return sign(data, process.env.ACCESS_SECRET, { expiresIn: 100 });
   },
-  verifyAccessToken: () => {}
+  isAuthorized: async (req) => {
+    const authorization = req.headers['authorization'];
+    if (!authorization) return null;
+    const token = authorization.split(' ')[1];
+    try {
+      return verify(token, process.env.ACCESS_SECRET);
+    } catch (err) {
+      return null;
+    }
+  }
 }
