e57fc863cfc79348eeb1dc5e40472b06504a19ca
edan_3000 <roseswallow13@gmail.com>
2021-08-27 16:44:01 +0900

    [Server] close #30 #34 feat:test list rending, select

--- server/controllers/testControllers.js
+++ server/controllers/testControllers.js
@@ -1,10 +1,22 @@
+const { select, test } = require('../models')
+
 module.exports = {
-  test: (req, res) => {
-    // const { email, password } = req.body;
-    return res.status(200).send('test');
+  test: async (req, res) => {
+    const testList = await test.findAll({
+      attributes: ['id', 'name']
+    });
+    return res.status(200).send({ data: {testList}, message:'ok' });
   },
-  select: (req, res) => {
+  select: async (req, res) => {
+    const testParamsId = req.params.id;
+
+    const result = await select.findAll({
+      attributes: ['id', 'name', 'image'],
+      where:{testId : testParamsId},
+    })
+    
+    console.log(result)
     // const { email, password } = req.body;
-    return res.status(200).send('select');
+    return res.status(200).send({ "data" : {"test" : result}, message: 'ok'});
   },
 }
