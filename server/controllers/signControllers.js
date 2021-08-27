26f41f38ef2cb4d1beaf4aca48ea2f5cedf83a0a
soyou <loverduck97@naver.com>
2021-08-27 13:58:25 +0900

    [Server] #7 feat: add login feature in signControllers

--- server/controllers/signControllers.js
+++ server/controllers/signControllers.js
@@ -1,9 +1,23 @@
 const { user } = require('../models')
+const { generateAccessToken } = require('../middleware/jwt')
 
 module.exports = {
-  login: (req, res) => {
-    // const { email, password } = req.body;
-    return res.status(200).send('login');
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    const userInfo = await user.findOne({ where: { email } });
+
+    if (!userInfo) {
+      return res.status(404).send({ message: "undefined user" });
+    } 
+
+    if (userInfo.dataValues.password !== password) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    delete userInfo.dataValues.password;
+    const accessToken = generateAccessToken(userInfo.dataValues);
+
+    return res.status(200).send({ data: { accessToken }, message: 'ok'});
   },
   loginGithub: () => {
     return res.status(200).send('github login');
---
+++ server/middleware/jwt.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const { sign, verify } = require('jsonwebtoken');
+
+module.exports = {
+  generateAccessToken: (data) => {
+    return sign(data, process.env.ACCESS_SECRET, { expiresIn: 100 });
+  },
+  verifyAccessToken: () => {}
+}
