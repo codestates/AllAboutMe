cda73bcf0a50d13782222f756f18d90ff7747b21
siyoon <younyaho@gmail.com>
2021-08-28 19:04:04 +0900

    [Client] #76 feat: add axios for login and signup

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,32 +1,73 @@
-import React from 'react';
-import Footer from './footer';
-import './login.css'
+import React, { useState } from "react";
+import axios from "axios";
+import Footer from "./footer";
+import "./login.css";
 
+axios.defaults.withCredentials = true;
 
-function login(){
+function Login() {
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("안녕하세요");
+  const handleInputValue = (key) => (e) => {
+    setLoginInfo({ ...loginInfo, [key]: e.target.value });
+  };
+  const handleLogin = () => {
+    const { email, password } = loginInfo;
+
+    if (!email || !password) {
+      setMessage("이메일과 비밀번호를 입력하세요");
+      return;
+    }
+
+    axios
+      .post(
+        "https://localhost:4000/signin",
+        { email, password },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        if (res.status === 201) console.log("login OK");
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="login-container">
-    <div className="login-logo">Login</div>
-        <input 
-        className="login-input"
-        placeholder="ID (email)"
-        type="email"
-        >
-        </input>
-    <div>
-        <input 
-        className="login-input"
-        placeholder="Password"
-        type="password"
-        >
-        </input>
-    </div>
-    <button className="login-button">Login</button>
-    <footer className='login_footer'>
+      <div className="login-logo">Login</div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          className="login-input"
+          placeholder="ID (email)"
+          type="email"
+          onChange={handleInputValue("email")}
+        ></input>
+        <div>
+          <input
+            className="login-input"
+            placeholder="Password"
+            type="password"
+            onChange={handleInputValue("password")}
+          ></input>
+        </div>
+        <div>
+          <div className="signup-notice-message-box">
+            <div>{message}</div>
+          </div>
+        </div>
+        <button className="login-button" type="submit" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+      <footer className="login_footer">
         <Footer />
-    </footer>
-</div>
-)
-};
+      </footer>
+    </div>
+  );
+}
 
-export default login;
+export default Login;
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -2,112 +2,134 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import Footer from './footer';
-import './signup.css'
+import './signup.css';
 
 axios.defaults.withCredentials = true;
 
-function SignUp(){
-    const [userInfo, setUserInfo] = useState({
-        email:'',
-        password:'',
-        username:'',
-        phone:''
-    });
-    const [message, setMessage] = useState('입력해주세요.');
+function SignUp() {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+    username: '',
+    phone: '',
+  });
+  const [message, setMessage] = useState('입력해주세요.');
 
-    const handleInputValue = (key) => (e) => {
-        setUserInfo({...userInfo, [key]: e.target.value});
+  const handleInputValue = (key) => (e) => {
+    setUserInfo({ ...userInfo, [key]: e.target.value });
+  };
+
+  const handleSignup = () => {
+    const { email, password, username, phone } = userInfo;
+    if (!email || !password || !username || !phone) {
+      setMessage('모든 항목은 필수입니다.');
+      return;
     }
 
-    const handleSignup = () => {
-        const {email, password, username, phone} = userInfo
-        if(!email || !password || !username || !phone) {
-            setMessage('모든 항목은 필수입니다.')
-            return ;
+    axios
+      .post(
+        'https://localhost:4000/signup',
+        { email, password, username, phone },
+        {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: true,
         }
+      )
+      .then((data) => {
+        if (data.status === 201) useHistory.push('/Login');
+      });
+  };
 
-        axios
-            .post('https://localhost:4000/signup',
-            { email, password, username, phone },
-            { headers: { 'Content-Type': 'application/json' }, withCredentials: true })
-            .then((data) => {
-                if(data.status === 201) useHistory.push('/Login')
-            })
-    }
+  function isEmail(asValue) {
+    var regExp =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    regExp.test(asValue) === false
+      ? setMessage('이메일 형식을 맞춰주세요.')
+      : setMessage('OK!');
+    //return regExp.test(asValue)
+  }
 
-    function isEmail(asValue) {
-        var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-        regExp.test(asValue) === false ? setMessage('이메일 형식을 맞춰주세요.') : setMessage('OK!')
-    }
+  function isCelluar(asValue) {
+    var regExp = /^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/;
+    regExp.test(asValue) === false
+      ? setMessage('휴대번호(010-####-####)형식을 맞춰주세요.')
+      : setMessage('OK!');
+    //return regExp.test(asValue)
+  }
 
-    function isCelluar(asValue) {
-        var regExp = /^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/;
-        regExp.test(asValue) === false ? setMessage('휴대번호(010-####-####)형식을 맞춰주세요.') : setMessage('OK!')
-    }
+  function isJobPassword(asValue) {
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
+    regExp.test(asValue) === false
+      ? setMessage(
+          '최소 8자 이상, 영문 대문자, 영문 소문자, 숫자, 특수문자가 각각 최소 1개 이상이여야 합니다.'
+        )
+      : setMessage('OK!');
+    //return regExp.test(asValue)
+  }
 
-    function isJobPassword(asValue) {
-        var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
-        regExp.test(asValue) === false ? setMessage('최소 8자 이상, 영문 대문자, 영문 소문자, 숫자, 특수문자가 각각 최소 1개 이상이여야 합니다.') : setMessage('OK!')
-    } 
-    
-    return (
-        <div className='signup-container'>
-            <div className='signup-logo'>Sign up</div>
-                <form onSubmit={(e)=>e.preventDefault()}>
-                    <div>
-                        <input 
-                            className='signup-input'
-                            placeholder='ID (email)'
-                            type='email'
-                            onChange={handleInputValue('email'), isEmail}
-                        />
-                    </div>
-                    <div>
-                        <input 
-                            className='signup-input'
-                            placeholder='Password'
-                            type='password'
-                            onChange={handleInputValue('password'), isJobPassword}
-                        >
-                        </input>
-                    </div>
-                    <div>
-                        <input 
-                            className='signup-input'
-                            placeholder='Name'
-                            type='name'
-                            onChange={handleInputValue('name')}
-                        >
-                        </input>
-                    </div>
-                    <div>
-                        <input 
-                            className='signup-input'
-                            placeholder='Phone Number'
-                            type='text'
-                            name='Phone'
-                            id='Phone'
-                            onChange={handleInputValue('phone'), isCelluar}
-                        >
-                        </input>
-                    </div>
-                </form>
-            <div>
-                <div className='signup-notice-message-box'>
-                    <div>{message}</div>
-                </div>
-            </div>
-            <div>
-                <div className='signup-checkbox-agree-container'>                    
-                    <input className='signup-checkbox' type='checkbox'/>
-                    <div className='signup-agree'>동의합니다.</div>  
-                </div>
-            </div>
-            <button className='signup-button' type='submit' onClick={isEmail || isCelluar|| isJobPassword ? handleSignup : !handleSignup}>Join us !</button>
-            <footer className='signup_footer'>
-                <Footer />
-            </footer>
+  return (
+    <div className='signup-container'>
+      <div className='signup-logo'>Sign up</div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <input
+            className='signup-input'
+            placeholder='ID (email)'
+            type='email'
+            onChange={(handleInputValue('email'), isEmail)}
+          />
         </div>
-    )
-};
-export default SignUp
+        <div>
+          <input
+            className='signup-input'
+            placeholder='Password'
+            type='password'
+            onChange={(handleInputValue('password'), isJobPassword)}
+          ></input>
+        </div>
+        <div>
+          <input
+            className='signup-input'
+            placeholder='Name'
+            type='name'
+            onChange={handleInputValue('name')}
+          ></input>
+        </div>
+        <div>
+          <input
+            className='signup-input'
+            placeholder='Phone Number'
+            type='text'
+            name='Phone'
+            id='Phone'
+            onChange={(handleInputValue('phone'), isCelluar)}
+          ></input>
+        </div>
+      </form>
+      <div>
+        <div className='signup-notice-message-box'>
+          <div>{message}</div>
+        </div>
+      </div>
+      <div>
+        <div className='signup-checkbox-agree-container'>
+          <input className='signup-checkbox' type='checkbox' />
+          <div className='signup-agree'>동의합니다.</div>
+        </div>
+      </div>
+      <button
+        className='signup-button'
+        type='submit'
+        onClick={
+          isEmail || isCelluar || isJobPassword ? handleSignup : !handleSignup
+        }
+      >
+        Join us !
+      </button>
+      <footer className='signup_footer'>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+export default SignUp;
