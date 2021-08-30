e03929efa391f569964d257b436ced0eee813814
soyou <loverduck97@naver.com>
2021-08-31 01:20:27 +0900

    test

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: 'include',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
   })
