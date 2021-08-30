bb7b1c49321a0f4ef0f045f32b088a2aaac7d7ae
soyou <loverduck97@naver.com>
2021-08-30 22:34:45 +0900

    [Server] feat: send jwt token cookie

--- server/controllers/jwt.js
+++ server/controllers/jwt.js
@@ -5,9 +5,19 @@ module.exports = {
   generateAccessToken: (data) => {
     return sign(data, process.env.ACCESS_SECRET, { expiresIn: '1h' });
   },
+  generateRefreshToken: (data) => {
+    return sign(data, process.env.REFRESH_SECRET, { expiresIn: '3d' });
+  },
   verifyAccessToken: (accessToken) => {
     try {
       return verify(accessToken, process.env.ACCESS_SECRET);
+    } catch (err) {
+      return null;
+    }
+  },
+  verifyRefreshToken: (refreshToken) => {
+    try {
+      return verify(refreshToken, process.env.REFRESH_SECRET);
     } catch (err) {
       return null;
     }
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'OPTIONS']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
   })
 );
 app.use(cookieParser());
