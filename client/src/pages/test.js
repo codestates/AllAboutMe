02d5d30ef103317efdc8c2d756e8746955990a8f
edan_3000 <roseswallow13@gmail.com>
2021-08-31 00:58:40 +0900

    test

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -3,23 +3,40 @@ import './test.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
+import axios from 'axios';
 
-function Test() {
+function Test({ category }) {
   const settings = {
     infinite: false,
     slidesToShow: 3,
     slidesToScroll: 1,
   };
+  
+  
 
-  const category = [
-    { id: 1, name: '카페', img: '/1starbucks.jpg' },
-    { id: 2, name: '운동', img: '/1dog.png' },
-    { id: 3, name: '동물', img: '/2dog.png' },
-    { id: 4, name: '한식', img: '/3dog.png' },
-    { id: 5, name: '운동', img: '/4dog.png' },
-    { id: 6, name: '동물', img: '/5dog.png' },
-  ];
-  const [current, setCurrent] = useState(0);
+  // const category = [
+  //   { id: 1, name: '카페', img: '/1starbucks.jpg' },
+  //   { id: 2, name: '운동', img: '/1dog.png' },
+  //   { id: 3, name: '동물', img: '/2dog.png' },
+  //   { id: 4, name: '한식', img: '/3dog.png' },
+  //   { id: 5, name: '운동', img: '/4dog.png' },
+  //   { id: 6, name: '동물', img: '/5dog.png' },
+  // ];
+
+  function loadTest () {
+    axios.get(
+      'http://localhost:4000/test',
+      {'Content-Type': 'application/json', 'withCredentials': true}
+    )
+    .then((data) => {
+      // console.log(data.data.data.testList);
+      let testList = data.data.data.testList;
+      console.log(testList)
+      category(testList)
+    })
+  }
+  
+  
   return (
     <div className='test'>
       <div className='test_container'>
