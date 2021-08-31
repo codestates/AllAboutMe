5f21933ab80e9837719aa59d39e13288817a2953
soyou <loverduck97@naver.com>
2021-08-31 13:13:01 +0900

    test

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const session = require('express-session')
 
 const app = express();
 const port = process.env.HTTP_PORT || 80;
@@ -20,6 +21,21 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
   })
 );
+
+app.use(session({
+  httpOnly: true,
+  secure: false, // https 환경에서만 session 정보를 주고받도록 처리
+  secret: '', // 암호화하는 데 쓰일 키
+  resave: false, // 세션을 언제나 저장할지 설정함
+  saveUninitialized: true, // 세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
+  cookie: { // 세션 쿠키 설정 (클라이언트에 보내는 쿠키 설정)
+    httpOnly: true,
+    secure: false
+  },
+
+}))
+
+
 app.use(cookieParser());
 
 // TODO: 미들웨어 jwt verify 함수 만들기
