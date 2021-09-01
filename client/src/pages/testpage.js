6c6171ce3b75f1bc9ed0b44c9ef725fc65002917
edan_3000 <roseswallow13@gmail.com>
2021-09-01 09:08:24 +0900

    [Client] #33 feat: image app

--- client/src/pages/testpage.js
+++ client/src/pages/testpage.js
@@ -2,77 +2,69 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './testpage.css';
 
-async function TestPage({ isTestid }) {
-  
-  const [category, setCategory] = useState([]);
-  const [imgs, setImgs] = useState([]);
-  const [display, setDisplay] = useState([]);
-  const [winners, setWinners] = useState([]);
-  const [remain, setRemain] = useState(category.length);
+
+function TestPage({ selectList }) {
+
+  const [category, setCategory] = useState(selectList);
+  const [remain, setRemain] = useState(0);
   const [roundName, setRoundName] = useState(['예선전']);
-  const [pairList, setPairList] = useState([]);
-  
-  
-  useEffect(() => {
-    category.sort(() => Math.random() - 0.5)
-    setImgs(category)
-    setDisplay([category[0], category[1]])
-    setRemain(category.length)
-  }, []);
-  
-  function clickHandler (img) {
 
-    if (img.length <= 2){
-      if (winners.length === 0){
-        setDisplay([img]);
-      }
-      else{
-        let updatedImg = [...winners, img];
-        setImgs(updatedImg);
-        setDisplay([updatedImg[0], updatedImg[1]]);
-        setWinners([]);
-      }
 
-    }else if(imgs.length > 2){
-      setWinners([...winners, img]);
-      setDisplay([imgs[2], imgs[3]]);
-      setImgs(imgs.slice(2));
-    }
-    setRemain(remain-1)
-    if(remain === 3){
-      setRoundName('결승전')
-    }
-    if(remain === 2){
-      setRoundName('당첨!')
-    }
-    console.log(winners)
-    console.log(imgs.length)
-    console.log('imgs',imgs)
+
+  // useEffect(() => {
+  //   category.sort(() => Math.random() - 0.5)
+  //   setImgs(category);
+  //   setDisplay([category[0], category[1]])
+  //   setRemain(category.length);
+  // }, []);
+
+
+  // function clickHandler (food) {
+
+  //   if (img.length <= 2){
+  //       if (winners.length === 0){
+  //           setDisplay([img]);
+  //         }
+  //         else{
+  //             let updatedImg = [...winners, img];
+  //             setImgs(updatedImg);
+  //             setDisplay([updatedImg[0], updatedImg[1]]);
+  //             setWinners([]);
+  //           }
+
+  //         }else if(imgs.length > 2){
+  //             setWinners([...winners, img]);
+  //             setDisplay([imgs[2], imgs[3]]);
+  //             setImgs(imgs.slice(2));
+  //           }
+  //           setRemain(remain-1)
+  //           if(remain === 3){
+  //               setRoundName('결승전')
+  //             }
+  //             if(remain === 2){
+  //                 setRoundName('당첨!')
+  //               }
+  //             console.log(winners)
+  // console.log(imgs.length)
+  // console.log('imgs',imgs)
+  // }
+
+  function clickEvent (data) {
+
   }
 
-  // select list 값 가져오기
-  
-  await axios.get(
-    `http://localhost:4000/test/${isTestid}`,
-  {'Content-Type': 'application/json', 'withCredentials': true}
-  )
-  .then((data) => {
-    let test = data.data.data.test;
-    console.log(test)
-    setCategory(test)
-  })
-  
 
+  
 
   return (
     <div className='testpage_container'>
       <div className='testpage_tournament_score'> 1/{remain}
-      </div>
+      </div>     
       <div className='testpage_match_name'>🥊 {roundName}</div>
       <div className='testpage_body_wrap'>
         {category.map(el => {
           return (
-            <div className='testpage_matchImg_matchBtn_container'>
+            <div key={el.id} className='testpage_matchImg_matchBtn_container'>
               <div className='testpage_matchImg_box'>
                 <img
                   className='testpage_matchImg'
@@ -81,7 +73,7 @@ async function TestPage({ isTestid }) {
                 />
               </div>
               <div>
-                <button className='testpage_btn' onClick={clickHandler(el)}>{el.name}</button>
+                <button className='testpage_btn' onClick={() => clickEvent(el)}>{el.name}</button>
               </div>
             </div>
           )
